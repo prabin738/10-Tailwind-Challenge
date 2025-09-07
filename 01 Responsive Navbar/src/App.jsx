64425b99d02ff52a714ed4bc740a694cad00a6b4
prabin738 <prabinsingh750@gmail.com>
2025-09-07 15:11:18 +0545

    few tweaks done

--- 01 Responsive Navbar/src/App.jsx
+++ 01 Responsive Navbar/src/App.jsx
@@ -7,14 +7,21 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
+  const handleMenu = () => {};
   return (
     <>
       <div>
-        <nav className="p-3 bg-white justify-between items-center">
+        <nav className="p-3 flex bg-white justify-between items-center">
           <a href="#" id="brand" className="flex gap-2 items-center">
             <img src={logo} alt="" className="max-w-12 max-h-12 object-cover" />
-            <span>To Desktop</span>
+            <span className="text-lg font-medium ">To Desktop</span>
           </a>
+          <button className="p-2" onClick={handleMenu}>
+            <i
+              className="fa-solid  fa-bars text-gray-600"
+              aria-hidden="true"
+            ></i>
+          </button>
         </nav>
       </div>
     </>
