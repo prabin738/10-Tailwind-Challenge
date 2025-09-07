5b08b463959eb36f0ce6a7c7744b62337e31b62e
prabin738 <prabinsingh750@gmail.com>
2025-09-07 17:43:45 +0545

    App setted up for responsive hero section also imported the navbar project 1 above hero section and installed tailwind and react icons

---
+++ 02 Responsive Hero Section/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./index.css";
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
---
+++ 02 Responsive Hero Section/tailwind.config.js
@@ -0,0 +1,21 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3238f2", // Your custom color
+      },
+    },
+  },
+  plugins: [],
+};
