5b682549e409aab3ef4327e07bdd4fd64fecdfa4
prabin738 <prabinsingh750@gmail.com>
2025-09-07 18:32:33 +0545

    few responsiveness done and rest is remaining

--- 02 Responsive Hero Section/tailwind.config.js
+++ 02 Responsive Hero Section/tailwind.config.js
@@ -7,13 +7,30 @@
 //   plugins: [],
 // }
 
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: "#3238f2", // Your custom color
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#3238f2", // Your custom color
+        primary: "#3238f2",
+      },
+      fontFamily: {
+        display: ["Poppins", "sans-serif"],
+        body: ["Inter", "sans-serif"],
       },
     },
   },
